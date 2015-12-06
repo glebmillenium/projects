807d428050ec0cb984e72908619d8d665f2543ed
glebmillenium <GodBlessYa95@gmail.com>
2015-12-06 21:40:33 +0800

    Fixed dpr projects normal and gamma distibution

--- Delphi/study_normal_and_gamma_distribution_and_build_graph_for_every/lab1/Лаба_1/MainLaba1.dfm
+++ Delphi/study_normal_and_gamma_distribution_and_build_graph_for_every/lab1/Лаба_1/MainLaba1.dfm
@@ -1,9 +1,9 @@
 object Form1: TForm1
-  Left = 134
-  Top = 119
-  Width = 1145
-  Height = 732
-  Caption = #1040#1085
+  Left = 233
+  Top = 150
+  Width = 856
+  Height = 622
+  Caption = #1060#1086#1088#1084#1072'_1'
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -18,177 +18,176 @@ object Form1: TForm1
   object PageControl1: TPageControl
     Left = 0
     Top = 0
-    Width = 1137
-    Height = 698
+    Width = 848
+    Height = 588
     ActivePage = TabSheet1
     Align = alClient
     TabOrder = 0
     object TabSheet1: TTabSheet
       Caption = #1042#1099#1095#1080#1089#1083#1077#1085#1080#1103
-      object Label2: TLabel
-        Left = 288
-        Top = 48
-        Width = 180
-        Height = 13
-        Caption = #1053#1086#1088#1084#1072#1083#1100#1085#1099#1081' '#1079#1072#1082#1086#1085' '#1088#1072#1089#1087#1088#1077#1076#1077#1083#1077#1085#1080#1103
-      end
-      object Label3: TLabel
-        Left = 264
-        Top = 136
-        Width = 3
-        Height = 13
-      end
-      object Label4: TLabel
-        Left = 56
-        Top = 280
-        Width = 115
-        Height = 13
-        Caption = #1043#1072#1084#1084#1072' '#1088#1072#1089#1087#1088#1077#1076#1077#1083#1077#1085#1080#1077
-      end
-      object Label5: TLabel
-        Left = 272
-        Top = 136
-        Width = 9
-        Height = 19
+      object Label1: TLabel
+        Left = 208
+        Top = 56
+        Width = 7
+        Height = 16
         Caption = 's'
         Font.Charset = SYMBOL_CHARSET
         Font.Color = clWindowText
-        Font.Height = -15
+        Font.Height = -13
         Font.Name = 'Symbol'
         Font.Style = []
         ParentFont = False
       end
-      object Label1: TLabel
-        Left = 272
-        Top = 104
-        Width = 6
-        Height = 13
-        Caption = 'n'
-      end
-      object Label6: TLabel
-        Left = 272
-        Top = 176
+      object Label2: TLabel
+        Left = 208
+        Top = 88
         Width = 8
         Height = 13
         Caption = 'm'
       end
-      object Label7: TLabel
-        Left = 272
-        Top = 216
+      object Label3: TLabel
+        Left = 208
+        Top = 24
+        Width = 6
+        Height = 13
+        Caption = 'n'
+      end
+      object Label4: TLabel
+        Left = 206
+        Top = 120
         Width = 6
         Height = 13
         Caption = 'k'
       end
+      object Label5: TLabel
+        Left = 8
+        Top = 8
+        Width = 180
+        Height = 13
+        Caption = #1053#1086#1088#1084#1072#1083#1100#1085#1099#1081' '#1079#1072#1082#1086#1085' '#1088#1072#1089#1087#1088#1077#1076#1077#1083#1077#1085#1080#1103
+      end
+      object Label6: TLabel
+        Left = 24
+        Top = 160
+        Width = 115
+        Height = 13
+        Caption = #1043#1072#1084#1084#1072' '#1088#1072#1089#1087#1088#1077#1076#1077#1083#1077#1085#1080#1077
+      end
       object Edit1: TEdit
-        Left = 296
-        Top = 96
+        Left = 224
+        Top = 24
         Width = 121
         Height = 21
         TabOrder = 0
         Text = 'Edit1'
       end
-      object Edit2: TEdit
-        Left = 296
-        Top = 136
-        Width = 121
-        Height = 21
-        TabOrder = 1
-        Text = 'Edit2'
-      end
-      object Edit3: TEdit
-        Left = 296
-        Top = 176
-        Width = 121
-        Height = 21
-        TabOrder = 2
-        Text = 'Edit3'
-      end
-      object Edit4: TEdit
-        Left = 296
-        Top = 216
-        Width = 121
-        Height = 21
-        TabOrder = 3
-        Text = 'Edit4'
-      end
       object Button1: TButton
-        Left = 40
-        Top = 88
-        Width = 169
+        Left = 0
+        Top = 24
+        Width = 161
         Height = 25
-        Caption = #1057#1086#1079#1076#1072#1090#1100' '#1074#1099#1073#1086#1088#1082#1091' '#1085#1077#1087#1088'.'
-        TabOrder = 4
+        Caption = #1057#1086#1079#1076#1072#1090#1100' '#1074#1099#1073#1086#1088#1082#1091' '#1085#1077#1086#1087#1088'. x'
+        TabOrder = 1
         OnClick = Button1Click
       end
       object Button2: TButton
-        Left = 40
-        Top = 128
-        Width = 169
+        Left = 0
+        Top = 56
+        Width = 161
         Height = 25
-        Caption = #1057#1086#1079#1076#1072#1090#1100' '#1074#1099#1073#1086#1088#1082#1091' '#1076#1080#1089#1082#1088'.'
-        TabOrder = 5
+        Caption = #1057#1086#1079#1076#1072#1090#1100' '#1074#1099#1073#1086#1088#1082#1091' '#1076#1080#1089#1082#1088'. x'
+        TabOrder = 2
         OnClick = Button2Click
       end
       object Button3: TButton
-        Left = 40
-        Top = 168
-        Width = 169
+        Left = 0
+        Top = 88
+        Width = 161
         Height = 25
         Caption = #1055#1086#1089#1090#1088#1086#1080#1090#1100' '#1075#1080#1089#1090#1086#1075#1088#1072#1084#1084#1091
-        TabOrder = 6
+        TabOrder = 3
         OnClick = Button3Click
       end
       object Button4: TButton
-        Left = 24
-        Top = 304
-        Width = 169
+        Left = 0
+        Top = 176
+        Width = 145
         Height = 25
-        Caption = #1057#1086#1079#1076#1072#1090#1100' '#1074#1099#1073#1086#1088#1082#1091' '#1085#1077#1087#1088'.'
-        TabOrder = 7
+        Caption = #1057#1086#1079#1076#1072#1090#1100' '#1074#1099#1073#1086#1088#1082#1091' '#1085#1077#1087#1088'. x'
+        TabOrder = 4
         OnClick = Button4Click
       end
       object Button5: TButton
-        Left = 24
-        Top = 352
-        Width = 169
+        Left = 0
+        Top = 208
+        Width = 145
         Height = 25
-        Caption = #1057#1086#1079#1076#1072#1090#1100' '#1074#1099#1073#1086#1088#1082#1091' '#1076#1080#1089#1082#1088'.'
-        TabOrder = 8
+        Caption = #1057#1086#1079#1076#1072#1090#1100' '#1074#1099#1073#1086#1088#1082#1091' '#1076#1080#1089#1082#1088'. x'
+        TabOrder = 5
         OnClick = Button5Click
       end
       object Button6: TButton
-        Left = 24
-        Top = 400
-        Width = 169
+        Left = 0
+        Top = 240
+        Width = 145
         Height = 25
         Caption = #1055#1086#1089#1090#1088#1086#1080#1090#1100' '#1075#1080#1089#1090#1086#1075#1088#1072#1084#1084#1091
-        TabOrder = 9
+        TabOrder = 6
         OnClick = Button6Click
       end
       object Button7: TButton
-        Left = 48
-        Top = 544
-        Width = 121
+        Left = 0
+        Top = 336
+        Width = 185
         Height = 25
         Caption = #1055#1086#1089#1090#1088#1086#1080#1090#1100' '#1075#1088#1072#1092#1080#1082#1080
-        TabOrder = 10
+        TabOrder = 7
         OnClick = Button7Click
       end
       object Button8: TButton
-        Left = 40
-        Top = 600
-        Width = 145
+        Left = 0
+        Top = 392
+        Width = 185
         Height = 25
         Caption = #1052#1077#1090#1086#1076#1080#1095#1077#1089#1082#1080#1077' '#1091#1082#1072#1079#1072#1085#1080#1103
+        TabOrder = 8
+      end
+      object Edit2: TEdit
+        Left = 224
+        Top = 56
+        Width = 121
+        Height = 21
+        TabOrder = 9
+        Text = 'Edit2'
+      end
+      object Edit3: TEdit
+        Left = 224
+        Top = 88
+        Width = 121
+        Height = 21
+        TabOrder = 10
+        Text = 'Edit3'
+      end
+      object Edit4: TEdit
+        Left = 224
+        Top = 120
+        Width = 121
+        Height = 21
         TabOrder = 11
+        Text = 'Edit4'
       end
       object StringGrid1: TStringGrid
-        Left = 664
+        Left = 368
         Top = 16
-        Width = 369
-        Height = 641
-        RowCount = 2
+        Width = 353
+        Height = 529
         TabOrder = 12
+        ColWidths = (
+          64
+          64
+          64
+          64
+          62)
       end
     end
     object TabSheet2: TTabSheet
@@ -197,15 +196,14 @@ object Form1: TForm1
       object Chart1: TChart
         Left = 0
         Top = 0
-        Width = 1129
-        Height = 670
+        Width = 840
+        Height = 560
         BackWall.Brush.Color = clWhite
         BackWall.Brush.Style = bsClear
+        Title.Frame.Color = 65408
         Title.Text.Strings = (
           'TChart')
         Title.Visible = False
-        Legend.Alignment = laBottom
-        Legend.Frame.Visible = False
         View3D = False
         Align = alClient
         PopupMenu = PopupMenu1
@@ -228,8 +226,8 @@ object Form1: TForm1
         object Series2: TFastLineSeries
           Marks.ArrowLength = 8
           Marks.Visible = False
-          SeriesColor = 16711808
-          LinePen.Color = 16711808
+          SeriesColor = -1
+          LinePen.Color = -1
           LinePen.Width = 2
           XValues.DateTime = False
           XValues.Name = 'X'
@@ -243,8 +241,8 @@ object Form1: TForm1
         object Series3: TFastLineSeries
           Marks.ArrowLength = 8
           Marks.Visible = False
-          SeriesColor = clLime
-          LinePen.Color = clLime
+          SeriesColor = 16744703
+          LinePen.Color = 16744703
           LinePen.Width = 2
           XValues.DateTime = False
           XValues.Name = 'X'
@@ -273,8 +271,8 @@ object Form1: TForm1
         object Series5: TFastLineSeries
           Marks.ArrowLength = 8
           Marks.Visible = False
-          SeriesColor = 4194432
-          LinePen.Color = 4194432
+          SeriesColor = clLime
+          LinePen.Color = clLime
           LinePen.Width = 2
           XValues.DateTime = False
           XValues.Name = 'X'
@@ -289,10 +287,10 @@ object Form1: TForm1
     end
   end
   object PopupMenu1: TPopupMenu
-    Left = 20
-    Top = 32
+    Left = 188
+    Top = 184
     object N1: TMenuItem
-      Caption = #1057#1082#1086#1087#1080#1088#1086#1074#1072#1090#1100' '#1074' '#1073#1091#1092#1077#1088#1085#1086#1084' '#1052#1052#1054
+      Caption = #1057#1082#1086#1087#1080#1088#1086#1074#1072#1090#1100' '#1074' '#1073#1091#1092#1077#1088'  '#1052#1052#1054
       OnClick = N1Click
     end
     object N2: TMenuItem
---
+++ Delphi/study_normal_and_gamma_distribution_and_build_graph_for_every/lab1/Лаба_1/MainLaba1.pas
@@ -0,0 +1,1064 @@
+unit MainLaba1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, ComCtrls, Math, StdCtrls, TeEngine, Series, Grids, ExtCtrls,
+  TeeProcs, Chart, Menus;  { Math - это математическая библиотека}    {rang g -находится в библеотике math}
+
+type
+  TForm1 = class(TForm)
+    PageControl1: TPageControl;
+    TabSheet1: TTabSheet;
+    Edit1: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    Button6: TButton;
+    Button7: TButton;
+    Button8: TButton;
+    Label4: TLabel;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Edit4: TEdit;
+    Label5: TLabel;
+    Label6: TLabel;
+    TabSheet2: TTabSheet;
+    Chart1: TChart;
+    StringGrid1: TStringGrid;
+    Series1: TFastLineSeries;
+    Series2: TFastLineSeries;
+    Series3: TFastLineSeries;
+    Series4: TFastLineSeries;
+    Series5: TFastLineSeries;
+    PopupMenu1: TPopupMenu;
+    N1: TMenuItem;
+    N2: TMenuItem;
+    procedure N1Click(Sender: TObject);
+    procedure FormResize(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button7Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+    procedure Button5Click(Sender: TObject);
+    procedure Button6Click(Sender: TObject);
+    procedure Button8Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+  { Массив вместо объявления переменной типа PChar }
+
+
+
+  ADF: array [0..255] of Char;
+
+ 
+
+  xm: array [1..100000] of double;
+
+
+
+  TitleText: ShortString;
+
+
+
+implementation
+
+{$R *.dfm}
+{$R+}
+
+  function Erlang(k:single; mi:integer; t:single):single;
+
+var z2,z3 : Extended;
+
+    i     : integer;
+
+begin
+
+                  { Гамма-функция - n факториал }
+
+  z2:=1;
+
+  if mi<2 then z2:=1 else for i:=1 to mi do z2:=z2*i;
+
+
+
+                { Экспоненциальный закон и Эрланг }
+
+
+
+  if mi<=0 then z3:=k else z3:=Power(k,mi+1)*Power(t,mi);
+
+
+
+  z3:=z3*exp(-k*t)/z2;
+
+ 
+
+  Erlang:=z3;
+
+procedure
+ TForm1.Button6Click(Sender: TObject);
+var i,j,k,n,code,Nmod,mm:integer; x,m,s,kk:double;
+
+    Xmin,Xmax,dx,y:Single;
+
+    xp: array [0..201] of integer;
+
+    t:ShortString;
+
+begin
+
+ 
+
+{ Построение гистограмм по выборкам Гамма распределенных случайных чисел }
+
+ 
+
+ TitleText:='Гистограммы гамма распределенных случайных чисел, гамма и нормальный закон распределения';
+
+ 
+
+ val(form1.Edit1.Text,n,code);
+
+ if code<>0  then n:=1000;
+
+ if n<10     then n:=10;
+
+ if n>100000 then n:=100000;
+
+ form1.Edit1.Text:=IntToStr(n);
+
+ 
+
+ val(form1.Edit2.Text,s,code);
+
+ if code<>0 then s:=10;
+
+ if s<0.001 then s:=0.001;
+
+ STR(s:5:3,t);
+
+ form1.Edit2.Text:=t;
+
+ 
+
+ val(form1.Edit3.Text,m,code);
+
+ if code<>0 then m:=20;
+
+ STR(m:5:3,t);
+
+ form1.Edit3.Text:=t;
+
+ 
+
+ val(form1.Edit4.Text,k,code);
+
+ if code<>0 then k:=100;
+
+ if k<10    then k:=10;
+
+ if k>200   then k:=200;
+
+ form1.Edit4.Text:=IntToStr(k);
+
+ 
+
+ kk:=m/SQR(s);
+
+ mm:=Round(m*kk-1);
+
+ 
+
+ Nmod:=Round(2*SQR(m)/SQR(s));
+
+ 
+
+ if Nmod=0 then Nmod:=1;
+
+ 
+
+ for i:=0 to form1.StringGrid1.RowCount-1 do
+
+ for j:=0 to form1.StringGrid1.ColCount-1 do form1.StringGrid1.Cells[j,i]:='';
+
+ 
+
+ form1.StringGrid1.RowCount:=2;
+
+ 
+
+ form1.StringGrid1.Cells[0,0]:='x';
+
+ form1.StringGrid1.Cells[1,0]:='y непр.  Г';
+
+ form1.StringGrid1.Cells[2,0]:='y дискр. Г';
+
+ form1.StringGrid1.Cells[3,0]:='y Г';
+
+ form1.StringGrid1.Cells[4,0]:='y G';
+
+ 
+
+ { х непрерывный }
+
+ 
+
+ for i:=1 to n do xm[i]:=GammaEE(Nmod,m);
+
+ 
+
+ Xmin:=xm[1];
+
+ Xmax:=xm[1];
+
+ for i:=1 to n do
+
+  begin
+
+   if Xmin>xm[i] then Xmin:=xm[i];
+
+   if Xmax<xm[i] then Xmax:=xm[i];
+
+  end;
+
+ dx:=(Xmax-Xmin)/k;
+
+ 
+
+ for j:=0 to k+1 do xp[j]:=0;
+
+ 
+
+ { Гистограмма }
+
+ 
+
+ for j:=1 to k+1 do
+
+ for i:=1 to n do
+
+ if (xm[i]>=Xmin+(j-1)*dx) and (xm[i]<Xmin+j*dx) then xp[j]:=xp[j]+1;
+
+ 
+
+ { Вывод в таблицу }
+
+ 
+
+ for j:=0 to k do
+
+  begin
+
+   if form1.StringGrid1.RowCount<j+1 then form1.StringGrid1.RowCount:=j+1;
+
+ 
+
+   x:=Xmin+j*dx-0.5*dx;
+
+   STR(x:7:5,t);
+
+   form1.StringGrid1.Cells[0,j+1]:=t;
+
+ 
+
+   y:=xp[j]/n/dx;
+
+   STR(y:7:5,t);
+
+   form1.StringGrid1.Cells[1,j+1]:=t;
+
+ 
+
+   if x>=0 then y:=Erlang(kk,mm,x) else y:=0;
+
+   STR(y:7:5,t);
+
+   form1.StringGrid1.Cells[3,j+1]:=t;
+
+ 
+
+   y:=exp(-SQR(x-m)/(2*SQR(s)))/SQRT(2*Pi)/s;
+
+   STR(y:7:5,t);
+
+   form1.StringGrid1.Cells[4,j+1]:=t;
+
+ 
+
+  end;
+
+ 
+
+ { х дискретный }
+
+ 
+
+ for j:=0 to k+1 do xp[j]:=0;
+
+ 
+
+ { Гистограмма }
+
+ 
+
+ for j:=1 to k+1 do
+
+ for i:=1 to n do
+
+ if (Round(xm[i])>=Xmin+(j-1)*dx) and (Round(xm[i])<Xmin+j*dx) then xp[j]:=xp[j]+1;
+
+ 
+
+ { Вывод в таблицу }
+
+ 
+
+ for j:=0 to k do
+
+  begin
+
+   x:=Xmin+j*dx-0.5*dx;
+
+   y:=xp[j]/n/dx;
+
+   STR(y:7:5,t);
+
+   form1.StringGrid1.Cells[2,j+1]:=t;
+
+  end;
+
+ 
+
+end;
+
+
+
+procedure TForm1.Button5Click(Sender: TObject);
+begin
+var i,j,k,Nmod:integer; x,m,s:double;
+
+    t:ShortString;
+
+begin
+
+
+
+{ Формирование Гамма распределенной выборки
+
+  дискретных по амплитуде случайных чисел }
+
+
+
+ TitleText:='График выброк случайных чисел';
+
+
+
+ val(form1.Edit1.Text,j,k);
+
+ if k<>0     then j:=1000;
+
+ if j<10     then j:=10;
+
+ if j>100000 then j:=100000;
+
+ form1.Edit1.Text:=IntToStr(j);
+
+ 
+
+ val(form1.Edit2.Text,s,k);
+
+ if k<>0    then s:=10;
+
+ if s<0.001 then s:=0.001;
+
+ STR(s:5:3,t);
+
+ form1.Edit2.Text:=t;
+
+
+
+ val(form1.Edit3.Text,m,k);
+
+ if k<>0 then m:=20;
+
+ STR(m:5:3,t);
+
+ form1.Edit3.Text:=t;
+
+
+
+ Nmod:=Round(2*SQR(s)/SQR(s));
+
+ if Nmod=0 then Nmod:=1;
+
+
+
+ form1.StringGrid1.Cells[0,0]:='№ п/п';
+
+ form1.StringGrid1.Cells[4,0]:='x диск. Г';
+
+
+
+ for i:=1 to j do
+
+  begin
+
+   x:=Round(GammaEE(Nmod,s));
+
+   if form1.StringGrid1.RowCount<i+1 then form1.StringGrid1.RowCount:=i+1;
+
+   form1.StringGrid1.Cells[0,i]:=IntToStr(i);
+
+   STR(x:4:2,t);
+
+   form1.StringGrid1.Cells[4,i]:=t;
+
+  end;
+end;
+
+ 
+
+
+
+procedure TForm1.N1Click(Sender: TObject);
+begin
+{ Копирование рисунка графика в буфер ММО }
+
+
+
+ form1.Chart1.CopyToClipboardMetafile(true); {заносим рисунок графика в буфер ММО}
+
+{это обработчик событий - вызов меню на экране}
+end;
+
+procedure TForm1.FormResize(Sender: TObject);
+begin
+   form1.StringGrid1.Left:=form1.Width-365;
+
+ form1.StringGrid1.Height:=form1.Height-65;
+
+ if form1.Width<720 then form1.Width:=720;
+
+
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+var i:integer;
+
+begin
+
+ 
+
+ form1.PageControl1.ActivePageIndex:=0; {включаем page control на первую закладку}
+
+ 
+
+ form1.Edit1.Text:='1000';           {вводим исходные значения}
+
+ form1.Edit2.Text:='10';
+
+ form1.Edit3.Text:='20';
+
+ form1.Edit4.Text:='150';
+
+ TitleText:='';      {загаловок графика}
+
+ 
+
+ for i:=0 to 4 do form1.Chart1.Series[i].ShowInLegend:=false;    {обращение графикам возможно по двум системам имен:
+ если графиков меньше 16 то к ним можно обращатья как к массиву}
+
+
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+var i,j,k:integer; x,m,s:double;
+
+    t:ShortString;
+
+begin
+
+ 
+
+{ Формирование нормально распределенной выборки случайных чисел }
+
+ 
+
+ TitleText:='График выброк случайных чисел';
+
+
+
+ val(form1.Edit1.Text,j,k);     {типовой алгоритм для ввода числа с экрана}
+
+ if k<>0     then j:=1000;    {если строка в число не преобразовывается то j=1000}
+
+ if j<10     then j:=10;      {если j меньше 10, то минимальное значение j=10 j-минимальное число эксп}
+
+ if j>100000 then j:=100000;     {если j больше 100000, то j=100000 - верхняя граница эксп}
+
+ form1.Edit1.Text:=IntToStr(j);   {в edit1 -вводим исправленное значение j}
+              {преобраз целое число в стр}   {j - число опытов}
+
+
+ val(form1.Edit2.Text,s,k);      {s - ср кв отклонение}
+
+ if k<>0    then s:=10;
+
+ if s<0.001 then s:=0.001;
+
+ STR(s:5:3,t); {преобразует число в строку, фомат числа 5 знаков, из них 3 после запятой}
+
+ form1.Edit2.Text:=t;         {если для целых чисел знаков не хватит, то они будут добав автоматич, но три знака после запятой будеть всегда}
+          {стр t выводим в edit2}
+
+
+ val(form1.Edit3.Text,m,k);
+
+ if k<>0 then m:=20; {m мат ожидание}
+
+ STR(m:5:3,t);
+
+ form1.Edit3.Text:=t;
+
+
+
+ form1.StringGrid1.Cells[0,0]:='№ п/п';   {stringGrid - текст таблица, Cells - ячейка}
+
+ form1.StringGrid1.Cells[1,0]:='x непр. G';  { 1,0 - первый столбец нулевой строки}
+
+
+              {наш эксперимент}
+ for i:=1 to j do
+
+  begin
+
+   x:=RandG(m,s);     {ген сл чисел, распред по норм зак, с мат ожид - m и ср кв откл s}
+
+   if form1.StringGrid1.RowCount<i+1 then form1.StringGrid1.RowCount:=i+1;  {число строк в таблице stringgrid}
+        {если число строк в таб < числа опытов, то мы прибавляем кней еще одну строку}      {нумерация строк всегда с нуля, потэтому на 1 больше, чем rowCount}
+   form1.StringGrid1.Cells[0,i]:=IntToStr(i); {в нулевой столбец i-ая строка - выводим номер опыта}
+
+   STR(x:7:5,t);
+
+   form1.StringGrid1.Cells[1,i]:=t;
+
+  end;
+
+end;
+
+
+
+
+
+procedure TForm1.Button2Click(Sender: TObject);
+
+var i,j,k:integer; x,m,s:double;
+
+    t:ShortString;
+
+begin
+
+ 
+
+{ Формирование нормально распределенной выборки
+
+  дискретных по амплитуде случайных чисел }
+
+ 
+
+ TitleText:='График выброк случайных чисел';
+
+ 
+
+ val(form1.Edit1.Text,j,k);
+
+ if k<>0     then j:=1000;
+
+ if j<10     then j:=10;
+
+ if j>100000 then j:=100000;
+
+ form1.Edit1.Text:=IntToStr(j);
+
+ 
+
+ val(form1.Edit2.Text,s,k);
+
+ if k<>0    then s:=10;
+
+ if s<0.001 then s:=0.001;
+
+ STR(s:5:3,t);
+
+ form1.Edit2.Text:=t;
+
+ 
+
+ val(form1.Edit3.Text,m,k);
+
+ if k<>0 then m:=20;
+
+ STR(m:5:3,t);
+
+ form1.Edit3.Text:=t;
+
+ 
+
+ form1.StringGrid1.Cells[0,0]:='№ п/п';
+
+ form1.StringGrid1.Cells[2,0]:='x дискр. G';
+
+ 
+
+ for i:=1 to j do
+
+  begin
+
+   x:=Round(RandG(m,s)); {операция округления до ближайшего целого}
+
+   if form1.StringGrid1.RowCount<i+1 then form1.StringGrid1.RowCount:=i+1;
+
+   form1.StringGrid1.Cells[0,i]:=IntToStr(i);
+
+   STR(x:4:2,t);
+
+   form1.StringGrid1.Cells[2,i]:=t;
+
+  end;
+
+end;
+
+ 
+
+procedure TForm1.Button3Click(Sender: TObject);
+
+var i,j,k,n,code:integer; x,m,s:double;
+
+    Xmin,Xmax,dx,y:Single;
+
+    xp: array [0..201] of integer;
+
+    t:ShortString;
+
+begin
+
+ 
+
+{ Построение гистограмм по выборкам нормально распределенных случайных чисел }
+
+ 
+
+ TitleText:='Гистограммы нормально распределенных случайных чисел и нормальный закон распределения';
+
+ 
+
+ val(form1.Edit1.Text,n,code);    {n - число опытов}
+ if code<>0  then n:=1000;
+
+ if n<10     then n:=10;
+
+ if n>100000 then n:=100000;
+
+ form1.Edit1.Text:=IntToStr(n);
+
+ 
+
+ val(form1.Edit2.Text,s,code);    {s - ввод ср кв откл}
+
+ if code<>0 then s:=10;
+
+ if s<0.001 then s:=0.001;
+
+ STR(s:5:3,t);
+
+ form1.Edit2.Text:=t;
+
+ 
+
+ val(form1.Edit3.Text,m,code);   { m- вводим мат ожидание }
+
+ if code<>0 then m:=20;
+
+ STR(m:5:3,t);
+
+ form1.Edit3.Text:=t;
+
+ 
+
+ val(form1.Edit4.Text,k,code);  { к- число интервалов гистаграммы}
+
+ if code<>0 then k:=100;
+
+ if k<10    then k:=10;
+
+ if k>200   then k:=200;
+
+ form1.Edit4.Text:=IntToStr(k);
+
+ 
+
+ for i:=0 to form1.StringGrid1.RowCount-1 do
+
+ for j:=0 to form1.StringGrid1.ColCount-1 do form1.StringGrid1.Cells[j,i]:='';
+
+ 
+
+ form1.StringGrid1.RowCount:=2;
+
+ 
+
+ form1.StringGrid1.Cells[0,0]:='x';
+
+ form1.StringGrid1.Cells[1,0]:='y непр.  G';
+
+ form1.StringGrid1.Cells[2,0]:='y дискр. G';
+
+ form1.StringGrid1.Cells[3,0]:='y G';
+
+ 
+
+ { х непрерывный }
+
+ 
+
+ for i:=1 to n do xm[i]:=RandG(m,s);
+
+ 
+
+ Xmin:=xm[1];
+
+ Xmax:=xm[1];
+
+ for i:=1 to n do
+
+  begin
+
+   if Xmin>xm[i] then Xmin:=xm[i];
+
+   if Xmax<xm[i] then Xmax:=xm[i];
+
+  end;
+
+ dx:=(Xmax-Xmin)/k;
+
+ 
+
+ for j:=0 to k+1 do xp[j]:=0;
+
+ 
+
+ { Гистограмма }
+
+ 
+
+ for j:=1 to k+1 do
+
+ for i:=1 to n do
+
+ if (xm[i]>=Xmin+(j-1)*dx) and (xm[i]<Xmin+j*dx) then xp[j]:=xp[j]+1;
+
+ 
+
+ for j:=0 to k do
+
+  begin
+
+   if form1.StringGrid1.RowCount<j+1 then form1.StringGrid1.RowCount:=j+1;
+
+ 
+
+   x:=Xmin+j*dx-0.5*dx;
+
+   STR(x:7:5,t);
+
+   form1.StringGrid1.Cells[0,j+1]:=t;
+
+ 
+
+   y:=xp[j]/n/dx;
+
+   STR(y:7:5,t);
+
+   form1.StringGrid1.Cells[1,j+1]:=t;
+
+ 
+
+   y:=exp(-SQR(x-m)/(2*SQR(s)))/SQRT(2*Pi)/s;
+
+   STR(y:7:5,t);
+
+   form1.StringGrid1.Cells[3,j+1]:=t;
+
+  end;
+
+ 
+
+ { х дискретный }
+
+ 
+
+ for j:=0 to k+1 do xp[j]:=0;
+
+ 
+
+ { Гистограмма }
+
+ 
+
+ for j:=1 to k+1 do
+
+ for i:=1 to n do
+
+ if (Round(xm[i])>=Xmin+(j-1)*dx) and (Round(xm[i])<Xmin+j*dx) then xp[j]:=xp[j]+1;
+
+ 
+
+ for j:=0 to k do
+
+  begin
+
+   x:=Xmin+j*dx-0.5*dx;
+
+   y:=xp[j]/n/dx;
+
+   STR(y:5:3,t);
+
+   form1.StringGrid1.Cells[2,j+1]:=t;
+
+  end;
+
+ 
+
+end;
+
+
+
+procedure TForm1.Button7Click(Sender: TObject);
+
+var i,j,k:integer; x,y:double;
+
+    t:ShortString;
+
+begin
+
+ 
+
+{ Построение графиков }
+
+ 
+
+ 
+
+ if form1.StringGrid1.RowCount<10 then exit;
+
+ 
+
+{ Очистка графиков, отключение легенд }
+
+ 
+
+ for i:=0 to 4 do
+
+  begin
+
+   form1.Chart1.Series[i].Clear;
+
+   form1.Chart1.Series[i].ShowInLegend:=false;
+
+  end;
+
+ 
+
+ { Вывод заголовка графика } 
+
+ 
+
+ form1.Chart1.Title.Visible:=True;
+
+ form1.Chart1.Title.Text.Clear;
+
+ form1.Chart1.Title.Text.Add(TitleText);
+
+ 
+
+ { Вывод легенд из таблицы }
+
+ 
+
+ for j:=1 to form1.StringGrid1.ColCount-1 do if form1.StringGrid1.Cells[j,0]<>'' then
+
+  begin
+
+   form1.Chart1.Series[j].Title:=form1.StringGrid1.Cells[j,0]+'   ';
+
+   form1.Chart1.Series[j].ShowInLegend:=True;
+
+  end;
+
+ 
+
+ { Рисование графиков из таблицы }
+
+ 
+
+ for i:=1 to form1.StringGrid1.RowCount-1 do
+
+  begin
+
+   t:=form1.StringGrid1.Cells[0,i];{переписываем содерж ячейки}
+
+   Val(t,x,k);
+
+   if k<>0 then {k<>0 строку в число преобраз невозможно}
+
+    begin
+
+     StrPCopy(ADF,'В строке '+IntToStr(i)+' ошибка по х!!!'); {преобраз в нуль строку, правила языка СИ,
+                                                               поэтому она не проверяет совместимость типов данных. Массив ADF исп как буфер}
+
+     Application.MessageBox(ADF,'Ошибка чтения таблицы',0); {функция Win32 API кот выводит стандартное окно сообщ. Ошибка чтения таблицы - заголово к формы.
+                                                               ADF содержимое формы. 0 на конце - число кнопок в табл, задается в двоичном виде.}
+
+     exit;
+
+    end;
+
+
+
+   for j:=1 to form1.StringGrid1.ColCount-1 do
+
+    begin
+
+     if form1.StringGrid1.Cells[j,0]<>'' then
+
+      begin
+
+       t:=form1.StringGrid1.Cells[j,i];
+
+       Val(t,y,k);
+
+       if k<>0 then
+
+        begin
+
+         StrPCopy(ADF,'В строке '+IntToStr(i)+' ошибка по y'+IntToStr(j)+'!!!');
+
+         Application.MessageBox(ADF,'Ошибка чтения таблицы',0);
+
+         exit;
+
+        end;
+
+       form1.Chart1.Series[j-1].AddXY(x,y); {присоединить еще одну точку к графику}
+
+      end;
+
+    end;
+
+  end;
+
+
+
+end;
+
+
+
+procedure TForm1.Button4Click(Sender: TObject);
+begin
+var i,j,k,Nmod:integer; x,m,s:double;
+
+    t:ShortString;
+
+begin
+
+
+
+{ Формирование Гамма распределенной выборки случайных чисел }
+
+
+
+ TitleText:='График выброк случайных чисел';
+
+
+
+ val(form1.Edit1.Text,j,k);
+
+ if k<>0     then j:=1000;
+
+ if j<10     then j:=10;
+
+ if j>100000 then j:=100000;
+
+ form1.Edit1.Text:=IntToStr(j);
+
+
+
+ val(form1.Edit2.Text,s,k);
+
+ if k<>0    then s:=10;
+
+ if s<0.001 then s:=0.001;
+
+ STR(s:5:3,t);
+
+ form1.Edit2.Text:=t;
+
+
+
+ val(form1.Edit3.Text,m,k);
+
+ if k<>0 then m:=20;
+
+ STR(m:5:3,t);
+
+ form1.Edit3.Text:=t;
+
+
+
+ Nmod:=Round(2*SQR(s)/SQR(s));
+
+ if Nmod=0 then Nmod:=1;
+
+
+
+ form1.StringGrid1.Cells[0,0]:='№ п/п';
+
+ form1.StringGrid1.Cells[3,0]:='x непр. Г';
+
+
+
+ for i:=1 to j do
+
+  begin
+
+   x:=GammaEE(Nmod,s);
+
+   if form1.StringGrid1.RowCount<i+1 then form1.StringGrid1.RowCount:=i+1;
+
+   form1.StringGrid1.Cells[0,i]:=IntToStr(i);
+
+   STR(x:7:5,t);
+
+   form1.StringGrid1.Cells[3,i]:=t;
+
+  end;
+
+end;
+
+end.
