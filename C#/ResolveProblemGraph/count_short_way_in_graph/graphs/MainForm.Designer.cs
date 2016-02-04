f18bdaa1d061e558b73ca48b98f0d73aacd7ad84
glebmillenium <GodBlessYa95@gmail.com>
2016-02-04 23:24:44 +0800

    Added new project for solving problem graph

---
+++ C#/ResolveProblemGraph/count_short_way_in_graph/graphs/MainForm.Designer.cs
@@ -0,0 +1,226 @@
+﻿/*
+ * Создано в SharpDevelop.
+ * Пользователь: Глеб
+ * Дата: 27.01.2016
+ * Время: 23:58
+ * 
+ * 
+ */
+namespace graphs
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.CheckBox checkBox2;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.CheckBox checkBox3;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label3;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.checkBox3 = new System.Windows.Forms.CheckBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(469, 323);
+			this.panel1.TabIndex = 0;
+			this.panel1.Click += new System.EventHandler(this.Panel1Click);
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.Appearance = System.Windows.Forms.Appearance.Button;
+			this.checkBox1.Location = new System.Drawing.Point(6, 33);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(158, 24);
+			this.checkBox1.TabIndex = 3;
+			this.checkBox1.Text = "Создать узел";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			this.checkBox1.Click += new System.EventHandler(this.CheckBox1Click);
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.Appearance = System.Windows.Forms.Appearance.Button;
+			this.checkBox2.Location = new System.Drawing.Point(6, 63);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(158, 24);
+			this.checkBox2.TabIndex = 4;
+			this.checkBox2.Text = "Создать связь";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			this.checkBox2.Click += new System.EventHandler(this.CheckBox2Click);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.checkBox3);
+			this.groupBox1.Controls.Add(this.checkBox2);
+			this.groupBox1.Controls.Add(this.checkBox1);
+			this.groupBox1.Location = new System.Drawing.Point(487, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(170, 132);
+			this.groupBox1.TabIndex = 5;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Панель управления";
+			// 
+			// checkBox3
+			// 
+			this.checkBox3.Appearance = System.Windows.Forms.Appearance.Button;
+			this.checkBox3.Location = new System.Drawing.Point(6, 93);
+			this.checkBox3.Name = "checkBox3";
+			this.checkBox3.Size = new System.Drawing.Size(158, 24);
+			this.checkBox3.TabIndex = 5;
+			this.checkBox3.Text = "Удалить компоненту";
+			this.checkBox3.UseVisualStyleBackColor = true;
+			this.checkBox3.Click += new System.EventHandler(this.CheckBox3Click);
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.button3);
+			this.groupBox2.Controls.Add(this.button2);
+			this.groupBox2.Controls.Add(this.button1);
+			this.groupBox2.Location = new System.Drawing.Point(493, 150);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(179, 111);
+			this.groupBox2.TabIndex = 6;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Действие:";
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(6, 77);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(158, 23);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "Построить остовное дерево";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(6, 48);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(158, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Провести раскраску графа";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(6, 19);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(158, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Найти оптимальный путь";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(499, 287);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(55, 20);
+			this.textBox1.TabIndex = 7;
+			this.textBox1.Text = "0";
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(499, 264);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(78, 20);
+			this.label1.TabIndex = 8;
+			this.label1.Text = "Начало графа";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(583, 264);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(74, 20);
+			this.label2.TabIndex = 10;
+			this.label2.Text = "Конец графа";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(583, 287);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(56, 20);
+			this.textBox2.TabIndex = 9;
+			this.textBox2.Text = "0";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(12, 352);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(592, 74);
+			this.label3.TabIndex = 11;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(715, 435);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.panel1);
+			this.Name = "MainForm";
+			this.Text = "graphs";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox2.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ C#/ResolveProblemGraph/count_short_way_in_graph/graphs/MainForm.cs
@@ -0,0 +1,207 @@
+﻿/*
+ * Создано в SharpDevelop.
+ * Пользователь: Глеб
+ * Дата: 27.01.2016
+ * Время: 23:58
+ * 
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.FSharp.Collections;
+using System.Linq;
+
+namespace graphs
+{
+	public partial class MainForm : Form
+	{
+		Snippet temp = null;
+		int count_node = 0;
+		List<Node> Nodes = new List<Node>();
+		List<SnippetConnect> Links = new List<SnippetConnect>();
+		
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		void Panel1Click(object sender, EventArgs e)
+		{
+			if (checkBox1.Checked)
+				drawNode();
+
+			if (checkBox2.Checked)
+				drawLine();
+
+			if (checkBox3.Checked)
+			{
+				
+			}
+		}
+
+		void CheckBox1Click(object sender, EventArgs e)
+		{
+			if (checkBox2.Checked || checkBox3.Checked)
+			{
+				checkBox2.Checked = false;
+				checkBox3.Checked = false;
+			}
+		}
+		void CheckBox2Click(object sender, EventArgs e)
+		{
+			if (checkBox1.Checked || checkBox3.Checked)
+			{
+				checkBox1.Checked = false;
+				checkBox3.Checked = false;
+			}
+		}
+		void CheckBox3Click(object sender, EventArgs e)
+		{
+			if (checkBox1.Checked || checkBox2.Checked)
+			{
+				checkBox1.Checked = false;
+				checkBox2.Checked = false;
+			}
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			int arg0 = Int32.Parse(textBox1.Text);
+			int arg1 = Int32.Parse(textBox2.Text);
+			
+			FSharpList<FSharpList<Tuple<int, int>>> arg2 = createList();
+				//Tuple<int, int> one = new Tuple<int, int>(1, 2);
+				//Tuple<int, int> two = new Tuple<int, int>(2, 7);
+				//Tuple<int, int> three = new Tuple<int, int>(2, 3);
+				//arg2 = SeqModule.ToList(new List<FSharpList<Tuple<int,int>>>{SeqModule.ToList(new List<Tuple<int,int>>(){one, two}), SeqModule.ToList(new List<Tuple<int,int>>(){three})});
+			FSharpList<int> arg3 = FSharpList<int>.Empty;
+			FSharpList<Tuple<FSharpList<int>, int>> result = Graph.search_short_way(arg0, arg1, arg2, arg3, arg0);
+			outputResult(result, arg0);
+		}
+
+		FSharpList<FSharpList<Tuple<int, int>>> createList()
+		{
+			FSharpList<Tuple<int, int>> list;
+			FSharpList<FSharpList<Tuple<int, int>>> result = 
+				FSharpList<FSharpList<Tuple<int, int>>>.Empty;
+			
+			for (int i = (count_node - 1); i >= 0; i--)
+			{
+				var subset = from Link in Links
+							 where Link.A == i
+							 select new Tuple<int, int>(Link.B, Int32.Parse(Link.line.Text));
+				list = SeqModule.ToList(subset);
+				result = new FSharpList<FSharpList<Tuple<int, int>>>(list, result);
+			}
+			
+			return result;
+		}
+		
+		void outputResult(FSharpList<Tuple<FSharpList<int>, int>> result, int start)
+		{
+			label3.Text = "";
+			foreach (var iter1 in result)
+			{
+				label3.Text += "Путь: " + start + " ";
+				foreach (var iter2 in iter1.Item1)
+				{
+					label3.Text += iter2 + " ";
+				}
+				label3.Text += "\n Длина:" + iter1.Item2 + "\n";
+			}
+		}
+		
+		void drawNode()
+		{
+			var coords_cursor_mouse = panel1.PointToClient(Control.MousePosition);
+			var circle = panel1.CreateGraphics();
+			var myPen = new Pen(Color.Black);
+			circle.DrawEllipse(myPen, 
+			                 new Rectangle(coords_cursor_mouse.X-15, 
+			                               coords_cursor_mouse.Y-15, 30, 30));
+			circle.DrawString(count_node.ToString(), 
+			                new Font("Arial", 10), 
+			                new SolidBrush(Color.Black), 
+			                new Rectangle(coords_cursor_mouse.X-7, 
+			                              coords_cursor_mouse.Y-7,
+			                              30, 30));
+			Node node = new Node(count_node, circle, coords_cursor_mouse.X-15, 
+			                     			coords_cursor_mouse.X+15,
+			                      			coords_cursor_mouse.Y-15, 				                               
+			                               	coords_cursor_mouse.Y+15,
+			                               	count_node.ToString());
+			Nodes.Add(node);
+			
+			this.count_node++;
+		}
+		
+		void drawLine()
+		{
+			var coords_cursor_mouse = panel1.PointToClient(Control.MousePosition);
+			foreach (Node node in Nodes)
+			{
+				if (node.belongToNode(coords_cursor_mouse.X, 
+				                      coords_cursor_mouse.Y))
+                {
+					if (this.temp == null) 
+					{
+						this.temp = new Snippet(){ 
+							zero = node.id,
+							first = node.centr.first,
+							second = node.centr.second,
+						};
+						checkBox1.Enabled = false;
+						checkBox2.Enabled = false;
+						checkBox3.Enabled = false;
+					}
+					else
+					{
+						var line = panel1.CreateGraphics();
+						var myPen = new Pen(Color.Black, 1);
+						
+						int pero_x1, pero_x2, pero_y1, pero_y2;
+						if (this.temp.first < node.centr.first)
+						{
+							pero_x1 = this.temp.first + 10;
+							pero_y1 = this.temp.second;
+							pero_x2 = node.centr.first - 10;
+			            	pero_y2 = node.centr.second;
+							line.DrawLine(myPen, this.temp.first + 10, this.temp.second, 
+							              node.centr.first - 10, node.centr.second);
+						}
+						else
+						{
+							pero_x1 = this.temp.first - 10;
+							pero_y1 = this.temp.second;
+							pero_x2 = node.centr.first + 10;
+			            	pero_y2 = node.centr.second;
+							line.DrawLine(myPen, this.temp.first - 10, this.temp.second, 
+							              node.centr.first + 10, node.centr.second);
+						}
+			
+			            double ugol = Math.Atan2(pero_x1 - pero_x2, pero_y1 - pero_y2);
+						
+			            line.DrawLine(myPen, pero_x2, pero_y2, Convert.ToInt32(pero_x2 + 15*Math.Sin(0.3 + ugol)), Convert.ToInt32(pero_y2 + 15*Math.Cos(0.3 + ugol)));
+			            line.DrawLine(myPen, pero_x2, pero_y2, Convert.ToInt32(pero_x2 + 15 * Math.Sin( ugol - 0.3)), Convert.ToInt32(pero_y2 + 15 * Math.Cos(ugol - 0.3)));
+			            var value = new TextBox();
+			            value.Size = new Size(20, 20);
+			            value.Location = new Point((this.temp.first + node.centr.first)/2,
+			                          (this.temp.second + node.centr.second)/2);
+			            panel1.Controls.Add(value);
+			            value.Text = "0";
+			            Links.Add(new SnippetConnect() { 
+			                      	A = this.temp.zero,
+			                      	B = node.id,
+			                      	line = value
+			                      });
+						checkBox1.Enabled = true;
+						checkBox2.Enabled = true;
+						checkBox3.Enabled = true;
+						this.temp = null;
+					}
+					break;
+                  }
+			}
+		}
+	}
+}
